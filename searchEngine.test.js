c5e60298cd6f21bc84e7a53b3f95131c1717cd59
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-06-15 17:33:49 +0300

    add real test

--- searchEngine.test.js
+++ searchEngine.test.js
@@ -1,6 +1,12 @@
 import { expect, test } from '@jest/globals';
-import sum from './searchEngine';
+import buildSearchEngine from './searchEngine';
 
-test('returns 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const docs = [doc1, doc2, doc3];
+const searchEngine = buildSearchEngine(docs);
+
+test('returns doc1 doc2', () => {
+  expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
 });
