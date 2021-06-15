748d9c0fbb16f7d6521105bea1095922c044e1f9
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-06-15 18:15:05 +0300

    add tests

--- searchEngine.test.js
+++ searchEngine.test.js
@@ -7,6 +7,11 @@ const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
 const searchEngine = buildSearchEngine(docs);
 
-test('returns doc1 doc2', () => {
+test('finds documents', () => {
   expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
+  expect(searchEngine.search('straight')).toEqual(['doc1']);
+  // expect(searchEngine.search('shooter')).toEqual(['doc3']);
+});
+test.skip('returns empty result', () => {
+  expect(searchEngine.search('hello')).toEqual([]);
 });
