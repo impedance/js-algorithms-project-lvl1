f6808e9ca1a8a8fcbfd2ab395237798c8b65c345
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-06-21 21:15:54 +0300

    move up comparing function

--- searchEngine.js
+++ searchEngine.js
@@ -1,7 +1,15 @@
+const isNeededPhrase = (word, phrase) => {
+  const normalizedWord = word.match(/\w+/g)[0];
+  const normalizedSearchPhrase = phrase.match(/\w+/g)[0];
+  return normalizedSearchPhrase === normalizedWord;
+};
 const buildSearchEngine = (documents) => ({
   search: (searchPhrase) => {
-    const filteredDocs = documents.filter(({ text }) => text.split(' ').includes(searchPhrase));
+    const filteredDocs = documents.filter(({ text }) => {
+      const words = text.split(' ');
+      return words.some((word) => isNeededPhrase(word, searchPhrase));
+    });
     return filteredDocs.map(({ id }) => id);
   },
 });
-export default buildSearchEngine;
+module.exports = buildSearchEngine;
