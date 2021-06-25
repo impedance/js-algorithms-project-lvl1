8bdca478223243b7c7f693f16863543c10eeb1ce
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-06-25 13:28:46 +0300

    add types and refactor

--- searchEngine.js
+++ searchEngine.js
@@ -1,15 +1,51 @@
-const hasSearchPhrase = (word, phrase) => {
-  const normalizedWord = word.match(/\w+/g)[0];
-  const normalizedPhrase = phrase.match(/\w+/g)[0];
-  return normalizedPhrase === normalizedWord;
+/**
+ * @param {string} word
+ * @returns string
+ */
+const getNormalizedWord = (word) => word.match(/\w+/g)[0];
+
+/**
+ * @param {string[]} content
+ * @param {string} phrase
+*/
+const getNumberOfMatches = (content, phrase) => {
+  let counter = 0;
+  for (let i = 0; i < content.length; i += 1) {
+    const cleanedWord = getNormalizedWord(content[i]);
+    if (phrase === cleanedWord) {
+      counter += 1;
+    }
+  }
+  return counter;
 };
+
+/**
+ * @param {{id: string, text: string}[]} documents
+ * @returns {{search: (arg0: string) => string[]}}
+ */
 const buildSearchEngine = (documents) => ({
+  /**
+   * @param {string} searchPhrase
+   * @returns {string[]}
+   */
   search: (searchPhrase) => {
-    const filteredDocs = documents.filter(({ text }) => {
+    if (documents.length === 0) {
+      return [];
+    }
+    const cleanedPhrase = getNormalizedWord(searchPhrase);
+    const matchedDocs = documents.filter(({ text }) => {
       const words = text.split(' ');
-      return words.some((word) => hasSearchPhrase(word, searchPhrase));
+      return words.some((word) => {
+        const cleanedWord = getNormalizedWord(word);
+        return cleanedWord === cleanedPhrase;
+      });
     });
-    return filteredDocs.map(({ id }) => id);
+    const relevanceResults = matchedDocs.reduce((acc, { id, text }) => {
+      const words = text.split(' ');
+      return { ...acc, [id]: getNumberOfMatches(words, cleanedPhrase) };
+    }, {});
+    console.log(relevanceResults);
+    return matchedDocs.map(({ id }) => id);
   },
 });
 module.exports = buildSearchEngine;
