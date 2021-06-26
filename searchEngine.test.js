99c46da584037db0842f397bed8f26cae57f11ec
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-06-26 19:18:02 +0300

    add test for relevance

--- searchEngine.test.js
+++ searchEngine.test.js
@@ -2,15 +2,15 @@ const { expect, test } = require('@jest/globals');
 const buildSearchEngine = require('./searchEngine');
 
 const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at I me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter. I'm master. I'm benefactor" };
 const docs = [doc1, doc2, doc3];
 const searchEngine = buildSearchEngine(docs);
 
 test('finds documents', () => {
-  expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
+  expect(searchEngine.search('shoot')).toEqual(['doc2', 'doc1']);
   expect(searchEngine.search('straight')).toEqual(['doc1']);
-  // expect(searchEngine.search('I')).toEqual(['doc3', 'doc1']);
+  expect(searchEngine.search('I')).toEqual(['doc3', 'doc1', 'doc2']);
   expect(searchEngine.search('shooter')).toEqual(['doc3']);
   expect(searchEngine.search('pint')).toEqual(['doc1']);
   expect(searchEngine.search('pint!')).toEqual(['doc1']);
