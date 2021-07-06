84b6b8b91b903d8e6ff841e9e12ca387a1637989
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-07-06 17:59:01 +0300

    add inverted index

--- searchEngine.js
+++ searchEngine.js
@@ -21,6 +21,24 @@ const getNumberOfMatches = (content, searchPhrases) => {
 };
 
 /**
+ * @param {{id: string, text: string}[]} docs
+ * @returns {{}}
+ */
+const getInvertedIndex = (docs) => docs.reduce((acc, { id, text }) => {
+  const pureWords = text.split(' ').map(getNormalizedWord);
+  const wordsToId = pureWords.reduce((coll, word) => ({ ...coll, [word]: [id] }), {});
+  let newAcc = { ...acc };
+  Object.keys(wordsToId).forEach((key) => {
+    if (newAcc[key]) {
+      newAcc = { ...newAcc, [key]: [...newAcc[key], ...wordsToId[key]] };
+    } else {
+      newAcc = { ...newAcc, [key]: [...wordsToId[key]] };
+    }
+  });
+  return newAcc;
+}, {});
+
+/**
  * @param {{id: string, text: string}[]} documents
  * @returns {{search: (arg0: string) => string[]}}
  */
@@ -37,18 +55,20 @@ const buildSearchEngine = (documents) => ({
     const phrases = searchPhrase.split(' ');
     const purePhrases = phrases.map(getNormalizedWord);
 
-    const matchedDocs = documents.filter(({ text }) => {
-      const words = text.split(' ');
-      return words.some((word) => {
-        const cleanedWord = getNormalizedWord(word);
-        return purePhrases.some((phrase) => phrase === cleanedWord);
-      });
+    const indexedWords = getInvertedIndex(documents);
+
+    const matchedKeys = Object.keys(indexedWords).filter((key) => {
+      return purePhrases.some(phrase => phrase === key);
     });
+
+    const matchedIds = matchedKeys.reduce((acc, key) => [...acc, ...indexedWords[key]], []);
+    const matchedDocs = documents.filter(({ id }) => matchedIds.includes(id));
 
     const relevanceResults = matchedDocs.reduce((acc, { id, text }) => {
       const words = text.split(' ');
       return [...acc, { id, count: getNumberOfMatches(words, phrases) }];
     }, []);
+
     relevanceResults.sort((a, b) => b.count - a.count);
 
     return relevanceResults.map(({ id }) => id);
