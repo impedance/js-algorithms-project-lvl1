738d265ed9a0f99ebce8847124eb974d9860a29c
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2021-07-27 21:15:56 +0300

    add tf idf calctulation

--- searchEngine.js
+++ searchEngine.js
@@ -4,31 +4,36 @@
  */
 const getNormalizedWord = (word) => word.match(/\w+/g)[0];
 
+// /**
+//  * @param {string} word
+//  * @param {Object} documents
+//  * @param {Object} indexedWords
+// */
+// const getWordRelevance = (word, documents, indexedWords) => {
+//   const result = indexedWords[word].docs.reduce((acc, { termFrequency }) => {
+// }, {});
+
 /**
- * @param {string[]} words
- * @param {string[]} searchPhrases
-*/
-const getNumberOfMatches = (words, searchPhrases) => {
-  const wordsQuantity = words.length;
-  let numberOfMatches = 0;
-  // let matchedDocsCount = 0;
-  for (let i = 0; i < wordsQuantity; i += 1) {
-    const currentWord = words[i];
-    const cleanedWord = getNormalizedWord(currentWord);
+ * @param {number} matchedDocsCount
+ * @param {number} docsCount
+ */
+const getInverseDocumentFrequency = (matchedDocsCount, docsCount) => {
+  return Math.log10(docsCount / matchedDocsCount).toPrecision(2);
+};
 
-    if (searchPhrases.includes(cleanedWord)) {
-      // matchedDocsCount = invertedIndex[cleanedWord].length;
-      numberOfMatches += 1;
+/**
+ * @param {string} word
+ * @param {string[]} phrases
+ * @returns number
+ */
+const getNumberOfMatches = (word, phrases) => {
+  let counter = 0;
+  phrases.forEach((phrase) => {
+    if (word === phrase) {
+      counter += 1;
     }
-  }
-
-  // const termFrequency = numberOfMatches / wordsQuantity;
-  // const idf = Math.fround(Math.log(docsNum / matchedDocsCount));
-  // console.log(docsNum, matchedDocsCount)
-  // console.log(Math.round(docsNum / matchedDocsCount));
-  // console.log(termFrequency, idf);
-  // const wordWeight = termFrequency * idf;
-  return numberOfMatches;
+  });
+  return counter;
 };
 
 /**
@@ -37,15 +42,20 @@ const getNumberOfMatches = (words, searchPhrases) => {
  */
 const getInvertedIndex = (docs) => docs.reduce((acc, { id, text }) => {
   const pureWords = text.split(' ').map(getNormalizedWord);
-  const wordsToId = pureWords.reduce((coll, word) => (
-    { ...coll, [word]: [{ id, count: getNumberOfMatches(pureWords, [word]) }] }
-  ), {});
+  const wordsToDocs = pureWords.reduce((coll, word) => {
+    const matchesCount = getNumberOfMatches(word, pureWords);
+    const wordsCount = pureWords.length;
+    const termFrequency = Number((matchesCount / wordsCount).toPrecision(2));
+
+    return { ...coll, [word]: { docs: [{ id, termFrequency }] } };
+  }, {});
   let newAcc = { ...acc };
-  Object.keys(wordsToId).forEach((key) => {
+  Object.keys(wordsToDocs).forEach((key) => {
+    const docsContainsWord = wordsToDocs[key];
     if (newAcc[key]) {
-      newAcc = { ...newAcc, [key]: [...newAcc[key], ...wordsToId[key]] };
+      newAcc = { ...newAcc, [key]: { docs: [...newAcc[key].docs, ...docsContainsWord.docs] } };
     } else {
-      newAcc = { ...newAcc, [key]: [...wordsToId[key]] };
+      newAcc = { ...newAcc, [key]: { docs: [...docsContainsWord.docs] } };
     }
   });
   return newAcc;
@@ -70,24 +80,45 @@ const buildSearchEngine = (documents) => ({
 
     const indexedWords = getInvertedIndex(documents);
 
-    const matchedDocs = purePhrases.reduce((acc, phrase) => {
-      if (indexedWords[phrase]) {
-        return [...acc, ...indexedWords[phrase]];
-      }
-      return [];
-    }, []);
-
-    const docsWithRelevance = matchedDocs.reduce((acc, { id, count }) => {
-      if (acc[id]) {
-        const { relevanceSum } = acc[id];
-        const newRelevanceSum = relevanceSum + count;
-        return { ...acc, [id]: { relevanceSum: newRelevanceSum } };
-      }
-      return { ...acc, [id]: { relevanceSum: count } };
+    const wordsWithIdf = Object.keys(indexedWords).reduce((acc, word) => {
+      const matchedDocsCount = indexedWords[word].docs.length;
+      const docsCount = documents.length;
+      const idf = getInverseDocumentFrequency(matchedDocsCount, docsCount);
+      return { ...acc, [word]: { docs: [...indexedWords[word].docs], idf } };
     }, {});
-    console.log(docsWithRelevance);
-    return Object.keys(docsWithRelevance).sort((a, b) => docsWithRelevance[b].relevanceSum - docsWithRelevance[a].relevanceSum);
 
+    const matchedDocs = purePhrases.reduce((acc, phrase) => {
+      if (wordsWithIdf[phrase]) {
+        const { idf, docs } = wordsWithIdf[phrase];
+        const idfiedDocs = docs.map(({ id, termFrequency }) => {
+          const tfIdf = (termFrequency * idf).toPrecision(2);
+          return { id, tfIdf };
+        });
+        // const sortedDocs = docs.sort((first, next) => {
+        //   return (next.termFrequency * idf) - (first.termFrequency * idf);
+        // });
+        return [...acc, ...idfiedDocs];
+      }
+      return acc;
+    }, []);
+    console.log(matchedDocs);
+
+    matchedDocs.map((doc) => {
+      if(acc[doc]) {
+        
+      }
+    });
+    // const docsWithRelevance = matchedDocs.reduce((acc, { id, count }) => {
+    //   if (acc[id]) {
+    //     const { relevanceSum } = acc[id];
+    //     const newRelevanceSum = relevanceSum + count;
+    //     return { ...acc, [id]: { relevanceSum: newRelevanceSum } };
+    //   }
+    //   return { ...acc, [id]: { relevanceSum: count } };
+    // }, {});
+    // return Object.keys(docsWithRelevance).sort((a, b) => {
+    //   return docsWithRelevance[b].relevanceSum - docsWithRelevance[a].relevanceSum;
+    // });
   },
 });
 module.exports = buildSearchEngine;
